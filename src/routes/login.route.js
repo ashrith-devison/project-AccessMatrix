f96cd37f10dcebe467867b59567410bb24155fa0
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-08-15 21:41:34 +0530

    chore: Remove login route file

--- src/routes/login.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { validate } = require('../controllers/verification.login.controller');
-const asyncHandler = require('../utils/asyncHandler');
-const router = express.Router();
-
-router.post('/login', asyncHandler(validate));
-
-module.exports = router;
