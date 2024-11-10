6d7044ff63b0eccae000591c0abf9dd88bc66201
Srujana Gayatri <srujanagayatri76@gmail.com>
2024-11-10 14:24:53 +0530

    Update AEP routes: change updateAEP method to use PATCH instead of PUT

--- src/routes/AEP.routes.js
+++ src/routes/AEP.routes.js
@@ -11,6 +11,6 @@ import {
 const router = Router();
 
 router.route('/').post(createAEP).get(getAEPs);
-router.route('/:id').get(getAEP).put(updateAEP).delete(deleteAEP);
+router.route('/:id').get(getAEP).patch(updateAEP).delete(deleteAEP);
 
 export default router;
